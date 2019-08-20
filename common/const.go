796a4fc4cf84d52fc5166ec262a5724c8b0dae29
chenzhijie <chenzhijie@onchain.com>
2019-08-20 11:00:47 +0800

    update syncing block offset to 50
    Signed-off-by: chenzhijie <chenzhijie@onchain.com>

--- common/const.go
+++ common/const.go
@@ -5,7 +5,7 @@ const (
 	DSP_URL_RAMDOM_NAME_LEN         = 8   // dsp custom name length
 	MAX_PROGRESS_INFO_NUM           = 100 // max progress info num for get all progress api
 	POLL_TX_COMFIRMED_TIMEOUT       = 10  // tx timeout
-	MAX_SYNC_HEIGHT_OFFSET          = 15  // max sync height offset
+	MAX_SYNC_HEIGHT_OFFSET          = 50  // max sync height offset
 	MAX_REG_CHANNEL_TIMES           = 5   // max register channel times
 	MAX_REG_CHANNEL_BACKOFF         = 10  // max register channel backoff
 	MAX_HEALTH_CHECK_INTERVAL       = 5   // health check interval
